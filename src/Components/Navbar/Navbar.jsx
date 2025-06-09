ae1559001218ea6c4cf229e1b74d22ede0a1fce9
Murilo justino <benatti1104@gmail.com>
2025-06-09 19:19:59 -0300

    use eeffect click on nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.jpg'
-import menu_icon from '../../assets/menu-icon.png'
+import React, { useEffect, useRef, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.jpg';
+import menu_icon from '../../assets/menu-icon.png';
 import { Link } from 'react-scroll';
 
-
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const menuRef = useRef();
 
-    const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 50);
+    };
 
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=>{
-            window.scrollY > 50 ? setSticky(true) : setSticky(false);
-        })
-    },[]);
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMobileMenu(false);
+      }
+    };
 
+    window.addEventListener('scroll', handleScroll);
+    document.addEventListener('mousedown', handleClickOutside);
 
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const toggleMenu = ()=>{
-      mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
-    }
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMobileMenu((prev) => !prev);
+  };
 
   return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-      <img src={logo} alt="" className='logo' />
-      <ul className={mobileMenu?'':'hide-mobile-menu'}>
-        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-        <li><Link to='about' smooth={true} offset={-150} duration={500}>About me</Link></li>
-        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Gallery</Link></li>
-              </ul>
-      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="" className="logo" />
+      <ul
+        ref={menuRef}
+        className={`nav-links ${mobileMenu ? 'show-mobile-menu' : 'hide-mobile-menu'}`}
+      >
+        <li>
+          <Link
+            to="hero"
+            smooth={true}
+            offset={0}
+            duration={500}
+            onClick={() => setMobileMenu(false)}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="about"
+            smooth={true}
+            offset={-150}
+            duration={500}
+            onClick={() => setMobileMenu(false)}
+          >
+            About me
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="campus"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            onClick={() => setMobileMenu(false)}
+          >
+            Gallery
+          </Link>
+        </li>
+      </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
