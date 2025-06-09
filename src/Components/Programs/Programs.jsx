98e751b0a6d132a4df7e1ccc420b63dcf819f242
Murilo justino <benatti1104@gmail.com>
2025-06-09 19:51:51 -0300

    New gallery

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Programs.css'
 import program_1 from '../../assets/gallery-4.png'
 import program_2 from '../../assets/gallery-2.png'
-import program_3 from '../../assets/gallery-3.png'
+import program_3 from '../../assets/gallery-5.png'
 import program_icon_1 from '../../assets/program-icon-1.png'
 import program_icon_2 from '../../assets/program-icon-2.png'
 import program_icon_3 from '../../assets/program-icon-3.png'
@@ -21,7 +21,7 @@ const Programs = () => {
         <img src={program_2} alt="" />
         <div className="caption">
             <img src={program_icon_2} alt="" />
-            <p>Couple</p>
+            <p>Couples</p>
         </div>
       </div>
       <div className="program">
