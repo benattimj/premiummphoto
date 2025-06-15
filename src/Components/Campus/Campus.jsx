2114661d2dac0e370655b068cfaf888d31cbec33
Murilo justino <benatti1104@gmail.com>
2025-06-14 21:24:42 -0300

    upload more photos

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -6,6 +6,18 @@ import gallery_4 from '../../assets/gallery-4.png'
 import gallery_6 from '../../assets/gallery-6.png'
 import gallery_7 from '../../assets/gallery-7.png'
 import gallery_8 from '../../assets/gallery-8.png'
+
+import gallery_20 from '../../assets/gallery-20.jpeg'
+
+import gallery_21 from '../../assets/gallery-21.jpeg'
+
+import gallery_22 from '../../assets/gallery-22.jpeg'
+import gallery_23 from '../../assets/gallery-23.jpeg'
+
+import gallery_24 from '../../assets/gallery-24.jpeg'
+
+import gallery_25 from '../../assets/gallery-25.jpeg'
+
 import white_arrow from '../../assets/white-arrow.png'
 
 const Campus = () => {
@@ -18,10 +30,19 @@ const Campus = () => {
         <img src={gallery_6} alt="" />
         <img src={gallery_7} alt="" />
         <img src={gallery_8} alt="" />
+        
+        <img src={gallery_20} alt="" />
+        <img src={gallery_21} alt="" />
+        <img src={gallery_22} alt="" />
+        <img src={gallery_23} alt="" />
+        <img src={gallery_24} alt="" />
+        
+        <img src={gallery_25} alt="" />
+
 
         
       </div>
-      <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
+      <button className='btn dark-btn'>See more here! <img src={white_arrow} alt="" /></button>
     </div>
   )
 }
